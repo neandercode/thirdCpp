5ac8b2f367041a9f695badb782461cf2070975d8
neandercode <josephjones555@yahoo.com>
2024-01-15 05:13:48 -0500

    first store variable

---
+++ storeVar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int yourFunction(int x, int y) {
+    return x + y;
+}
+
+int main() 
+{
+    cout << yourFunction(10, 18);
+    return 0;
+}
