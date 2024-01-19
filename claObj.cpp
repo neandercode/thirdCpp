4601f956d70dcd3a80f636fb3ad6f034f825a6ee
neandercode <josephjones555@yahoo.com>
2024-01-19 04:34:04 -0500

    constructor first commit

--- claObj.cpp
+++ claObj.cpp
@@ -2,14 +2,16 @@
 #include <string>
 using namespace std;
 
-class Car {
-    public:
-        string brand;
-        string model;
-        int year;
+class Car
+{
+public:
+    string brand;
+    string model;
+    int year;
 };
 
-int main() {
+int main()
+{
     Car carObj1;
     carObj1.brand = "Chevy";
     carObj1.model = "Camaro";
@@ -20,11 +22,8 @@ int main() {
     carObj2.model = "GT";
     carObj2.year = 2024;
 
-    cout << carObj1.brand << " " << carObj1.model << " " <<
-carObj1.year << "\n"; 
-    cout << carObj2.brand << " " << carObj2.model << " " <<
-carObj2.year << "\n";
-
+    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
 
     return 0;
 }
---
+++ constructor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class myClass{
+    public:
+        myClass() {
+            cout << "Hello World!";
+        }
+
+};
+
+int main()
+{
+    myClass myObj;
+
+    return 0;
+}
