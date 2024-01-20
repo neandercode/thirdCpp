08cabe4b63538a7fe4b04cb9b2e3b769f5e68fd5
neandercode <josephjones555@yahoo.com>
2024-01-20 07:19:19 -0500

    getline commit

---
+++ getLine.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string fullName;
+    cout << "Type in your full name: ";
+    getline (cin, fullName);
+    cout << "Se llama es: ";
+    return 0;
+}
