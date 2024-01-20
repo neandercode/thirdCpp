b24be12f81ad4c8d49b923cc8dc5b7d992c4039a
neandercode <josephjones555@yahoo.com>
2024-01-19 21:02:29 -0500

    boolean first commit

---
+++ boolean.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    bool isCodingFun = true;
+    bool isKaleDelicious = false;
+
+    cout << isCodingFun << " \n";
+    cout << isKaleDelicious;
+    return 0;
+}
