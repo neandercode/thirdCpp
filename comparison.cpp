b16b2ddad226dc30ca6a28f2dcc6a037f4e9526c
neandercode <josephjones555@yahoo.com>
2024-01-20 05:39:55 -0500

    comparison first commit

---
+++ comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int x = 4;
+    int y = 9;
+    cout << (x > y);
+    return 0;
+}
+
+//terminal returns 0 because x is less than y
