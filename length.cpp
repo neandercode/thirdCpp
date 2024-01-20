78ecd6608e91f552733101142ee4a8bd2e8db1fd
neandercode <josephjones555@yahoo.com>
2024-01-20 07:00:34 -0500

    length commit

---
+++ length.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the string is: " << txt.length();
+    return 0;
+}
