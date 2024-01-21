027c28afa5c6c0cda8d5f7df5bb6798adef2e9ba
neandercode <josephjones555@yahoo.com>
2024-01-21 07:06:44 -0500

    if else first

---
+++ ifElse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int time = 20;
+    if (time < 18) {
+        cout << "Good day.";
+    } else {
+        cout << "Good evening.";
+    }
+    return 0;
+}
