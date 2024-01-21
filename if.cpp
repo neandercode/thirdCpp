b3a9ceaf05bea1a3bd29247bab4c53562f00d800
neandercode <josephjones555@yahoo.com>
2024-01-21 06:58:26 -0500

    if statement

---
+++ if.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int x = 30;
+    int y = 12;
+    if (x > y) 
+    {
+        cout << "x is greater than y";
+    }
+    return 0;
+}
