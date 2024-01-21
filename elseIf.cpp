5bb2e42330a68faf35ffd4b3a4db2061d882f028
neandercode <josephjones555@yahoo.com>
2024-01-21 07:32:39 -0500

    else if first commit

---
+++ elseIf.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int time = 22;
+    if (time < 10) {
+        cout << "Good Morning.";
+    } else if (time < 20) {
+        cout << "Good Day.";
+    } else {
+        cout << "Good Evening.";
+    }
+    return 0;
+}
