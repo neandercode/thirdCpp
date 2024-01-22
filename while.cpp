eb90380bf61db42f54e34cb997dbf9257c7c0868
neandercode <josephjones555@yahoo.com>
2024-01-22 05:17:44 -0500

    while loop

---
+++ while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int i = 0;
+    while (i < 21) {
+        cout << i << "\n";
+        i++;
+    }
+
+    return 0;
+}
