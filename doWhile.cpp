1821b7d50b522f9ee22e86bc08fd2c7220c5cafa
neandercode <josephjones555@yahoo.com>
2024-01-22 05:37:06 -0500

    do while loop

---
+++ doWhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    do {
+        cout << i << "\n";
+        i++;
+    }
+    while (i < 5);
+
+    return 0;
+}
