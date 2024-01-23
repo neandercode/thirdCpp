1c930796a5270416b631ff3fb95c9b92535847a1
neandercode <josephjones555@yahoo.com>
2024-01-23 18:51:29 -0500

    multInheritance

---
+++ multipleInheritance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MyClass {
+    public:
+        void myFunction() {
+            cout << "Some content in the parent class." ;
+        }
+
+};
+
+class MyOtherClass {
+    public:
+        void myOtherFunction() {
+            cout << "Some content in another class." ;
+        }
+
+};
+
+class MyChildClass: public MyClass, public MyOtherClass {
+
+};
+
+int main()
+{
+    MyChildClass myObj;
+    myObj.myFunction();
+    myObj.myOtherFunction();
+    return 0;
+
+}
